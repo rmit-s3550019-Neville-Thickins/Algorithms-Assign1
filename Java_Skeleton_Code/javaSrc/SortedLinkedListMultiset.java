5d1e5b7c4ab29e5fa7752a5ee22b965e6ef6bcb2
JayMales <30455994+JayMales@users.noreply.github.com>
2017-08-24 22:06:03 +1000

    Update SortedLinkedListMultiset.java

--- Java_Skeleton_Code/javaSrc/SortedLinkedListMultiset.java
+++ Java_Skeleton_Code/javaSrc/SortedLinkedListMultiset.java
@@ -1,38 +1,63 @@
 import java.io.PrintStream;
 import java.util.*;
 
-public class SortedLinkedListMultiset<T> extends Multiset<T>
+
+public class SortedLinkedListMultiset<T> extends LinkedListMultiset<T>
 {
 	public SortedLinkedListMultiset() {
-		// Implement me!
+		super();
 	} // end of SortedLinkedListMultiset()
 	
 	
 	public void add(T item) {
-		// Implement me!
+		super.add(item);
+		if(msLength > 1) {
+			sort();
+		}
 	} // end of add()
-	
-	
-	public int search(T item) {
-		// Implement me!		
-		
-		// default return, please override when you implement this method
-		return 0;
-	} // end of add()
-	
 	
 	public void removeOne(T item) {
-		// Implement me!
-	} // end of removeOne()
+		super.removeOne(item);
+		if(msLength > 1) {
+			sort();
+		}
+	}
+	public void sort() {
+		boolean swap = false;
+		int compare =0;
+		int count = msLength;
+		
+		do {
+			Node sort = msHead;
+			swap = false;
+			for(int i=0; i <= count; i++) {
+				if(sort.getNext() !=null) {
+					compare = sort.getValue().toString().compareTo(sort.getNext().getValue().toString());
+					if(compare > 0) {
+						swapem(sort, sort.getNext());
+						swap = true;
+					}
+					if(sort.getNext() != null) {
+						sort = sort.getNext();
+					}else {
+						break;
+					}
+				}
+			}
+		}while(swap);
+	}
 	
-	
-	public void removeAll(T item) {
-		// Implement me!
-	} // end of removeAll()
-	
-	
-	public void print(PrintStream out) {
-		// Implement me!
-	} // end of print()
-	
+	public void swapem(Node first, Node second) {
+		Node p = first.getPrev();
+		Node n = second.getNext();
+		if(p != null)
+			p.setNext(second);
+		if(first.equals(msHead))
+			msHead = second;
+		second.setNext(first);
+		second.setPrev(first.getPrev());
+		first.setNext(n);
+		first.setPrev(second);
+		
+	}
 } // end of class SortedLinkedListMultiset
